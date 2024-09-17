20fdbb2fdb47ed317df7f04f10309817e7a8d5b6
Sumin Kim <zzmzzm0065@gmail.com>
2024-09-17 14:16:46 +0900

    configure: add alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // alias를 사용하기위해 tsconfig에 몇가지 설정을 추가해야해
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@routes': fileURLToPath(new URL('./src/routes', import.meta.url)),
+    }
+  }
 })
